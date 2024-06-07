be554012002c114dddc31c3017b591219a42af93
smit-savaliya <smit.savaliya1717@gmail.com>
2024-06-07 20:30:45 +0530

    change in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart'element={<Cart/>} />
-        <Route path='/order' element={<PlaceOrder/>} />
+        <Route path='/order' element={<PlaceOrder/>} /> 
       </Routes>
     </div>
   )
