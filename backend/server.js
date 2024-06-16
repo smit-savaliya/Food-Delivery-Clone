5a3cb0b8560e3004bc6c16d0b6aa42dc534a96a8
smit-savaliya <smit.savaliya1717@gmail.com>
2024-06-16 20:45:41 +0530

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,23 @@ const app = express()
 
 //middleware
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+
+const allowedOrigins = [
+    "https://tomato-wx0c.onrender.com", // replace with your actual food app URL
+    "https://admin-tomato.onrender.com" // replace with your actual admin panel URL
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 //api endpoints
 app.use("/api/food",foodRoter)
