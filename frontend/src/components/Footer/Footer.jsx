386828a2ac1f12b09cacd5df986a3ecf89ac87ab
smit-savaliya <smit.savaliya1717@gmail.com>
2024-06-17 10:43:07 +0530

    change footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -36,7 +36,7 @@ function Footer() {
             </div>
             <hr />
             <p className='footer-copyright'>
-                Copyright 2024 Tomato.com -All Right Reserved.
+                Copyright -c  2024 Tomato.com -All Right Reserved.
             </p>
         </div>
     )
