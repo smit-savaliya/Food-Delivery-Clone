816981ff33c4d4944062104a6d68987283ed0e55
smit-savaliya <smit.savaliya1717@gmail.com>
2024-10-25 01:50:55 +0530

    add myorder

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,9 +45,9 @@ const placeorder = async (req,res)=>{
         const session = await stripe.checkout.sessions.create({
             line_items:line_itmes,
             mode:"payment",
-            success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            success_url:`${frontend_url}/myorders`,
 
-            cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+            cancel_url:`${frontend_url}/cart`,
 
         })
 
